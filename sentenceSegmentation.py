e8291bc6fd2a015f54eb858ae94d947e0f60dc18
vakada naveen <vakadanaveen@gmail.com>
2021-03-05 15:07:34 +0530

    Added code snubs

---
+++ sentenceSegmentation.py
@@ -0,0 +1,71 @@
+from util import *
+
+# Add your import statements here
+import re
+import nltk.data
+
+
+class SentenceSegmentation():
+
+	def naive(self, text):
+		"""
+		Sentence Segmentation using a Naive Approach
+
+		Parameters
+		----------
+		arg1 : str
+			A string (a bunch of sentences)
+
+		Returns
+		-------
+		list
+			A list of strings where each string is a single sentence
+		"""
+
+		segmentedText = None
+
+		#Fill in code here
+		a=re.split('([!\?.;:"]+)',text)
+
+		b=[]
+		#print('a',a)
+		for i in range(0,len(a)-1,2):
+			b.append(a[i].strip()+a[i+1].strip())
+		if (len(a)%2)==1:b.append(a[-1].strip())
+		segmentedText = list(filter(lambda x: True if len(x) > 0 else False, b))
+
+		return segmentedText
+
+
+
+
+
+	def punkt(self, text):
+		"""
+		Sentence Segmentation using the Punkt Tokenizer
+
+		Parameters
+		----------
+		arg1 : str
+			A string (a bunch of sentences)
+
+		Returns
+		-------
+		list
+			A list of strings where each strin is a single sentence
+		"""
+
+		segmentedText = None
+
+		#Fill in code here
+		sent_detector = nltk.data.load('tokenizers/punkt/english.pickle')
+		segmentedText=sent_detector.tokenize(text.strip())
+
+		return segmentedText
+s = SentenceSegmentation()
+'''print(s.naive("Hello world! how are you?"))
+print(s.punkt("Hello world! how are you?"))
+print('Here naive fails: ',s.naive("Hello world! I have $10.67"))
+print('punkt:',s.punkt("Hello world! I have $10.67"))
+print(s.punkt("hello world!!! how Are You? I am (having a Party' 's) 1.1_2 ' s"))
+print(s.naive("Hello world!!! how Are You? I am (having a party) 1.1.2 fig"))'''
---
+++ tokenization.py
@@ -0,0 +1,64 @@
+from util import *
+
+# Add your import statements here
+
+
+import re
+from nltk.tokenize import TreebankWordTokenizer
+
+class Tokenization():
+
+	def naive(self, text):
+		"""
+		Tokenization using a Naive Approach
+
+		Parameters
+		----------
+		arg1 : list
+			A list of strings where each string is a single sentence
+
+		Returns
+		-------
+		list
+			A list of lists where each sub-list is a sequence of tokens
+		"""
+
+		tokenizedText = None
+
+		#Fill in code here
+		a=[]
+		for txt in text:
+			l=re.split('([!\?.;:\s,()"])', txt)
+			l=list(map(lambda x:x.strip(),l))
+			l=list(filter(lambda x: True if len(x) > 0 else False, l))
+			a.append(l)
+
+		tokenizedText=a
+		return tokenizedText
+
+
+
+	def pennTreeBank(self, text):
+		"""
+		Tokenization using the Penn Tree Bank Tokenizer
+
+		Parameters
+		----------
+		arg1 : list
+			A list of strings where each string is a single sentence
+
+		Returns
+		-------
+		list
+			A list of lists where each sub-list is a sequence of tokens
+		"""
+
+		tokenizedText = None
+
+		#Fill in code here
+		tokenizedText=TreebankWordTokenizer().tokenize(text)
+
+		return tokenizedText
+t=Tokenization()
+#print(t.naive(["Hello world! how are you?","Hai what is this"]))
+#print(t.pennTreeBank("Hello world! (how) are you?"))
